29b51c3c27f8ac2979f15de9f9f18639666b95cf
02Naitsirk <57512128+02Naitsirk@users.noreply.github.com>
2019-12-20 13:08:27 -0500

    Use struct for osu note, implement ez and ht

--- osuDifficultyCalculator/Beatmap.cs
+++ osuDifficultyCalculator/Beatmap.cs
@@ -1,3 +1,5 @@
+using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace osuDifficultyCalculator
@@ -5,19 +7,98 @@ namespace osuDifficultyCalculator
     public class Beatmap
     {
         public double circleSize, overallDifficulty, approachRate,
-        starRating, HRstarRating, DTstarRating, DTHRstarRating,
-        nmPP, hdPP, hrPP, hdhrPP, dtPP, dthdPP, dthrPP, dthdhrPP;
+            ezhtStarRating, nmhtStarRating, hrhtStarRating,
+            ezStarRating, nmStarRating, hrStarRating,
+            ezdtStarRating, nmdtStarRating, hrdtStarRating;
+
+        public int circleCount, objectCount;
+
         public string title, artist, creator, version;
-        public List<int> xCoordinates = new List<int>();
-        public List<int> yCoordinates = new List<int>();
-        public List<int> timeCoordinates = new List<int>();
-        public List<int> objectTypes = new List<int>();
-        public List<double> strains = new List<double>();
-        public List<double> HRstrains = new List<double>();
-        public List<double> DTstrains = new List<double>();
-        public List<double> DTHRstrains = new List<double>();
-        public List<double> distances = new List<double>();
-        public List<double> timeDifferences = new List<double>();
-        public double circleCount;
+
+        public List<Note> osuNotes = new List<Note>();
+
+        public List<double> ezhtDifficulties = new List<double>();
+        public List<double> nmhtDifficulties = new List<double>();
+        public List<double> hrhtDifficulties = new List<double>();
+
+        public List<double> ezDifficulties = new List<double>();
+        public List<double> nmDifficulties = new List<double>();
+        public List<double> hrDifficulties = new List<double>();
+
+        public List<double> ezdtDifficulties = new List<double>();
+        public List<double> nmdtDifficulties = new List<double>();
+        public List<double> hrdtDifficulties = new List<double>();
+
+        public struct Note
+        {
+            public int xCoordinate, yCoordinate, time, objectType;
+            public Note (int x, int y, int t, int o)
+            {
+                xCoordinate = x;
+                yCoordinate = y;
+                time = t;
+                objectType = o;
+            }
+        }
+
+        public void GetBeatmapData(string fileName)
+        {
+            string line;
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains("Title:"))
+                    {
+                        title = line.Split(new[] { ':' }, 2)[1];
+                    }
+                    if (line.Contains("Artist:"))
+                    {
+                        artist = line.Split(new[] { ':' }, 2)[1];
+                    }
+                    if (line.Contains("Creator:"))
+                    {
+                        creator = line.Split(new[] { ':' }, 2)[1];
+                    }
+                    if (line.Contains("Version:"))
+                    {
+                        version = line.Split(new[] { ':' }, 2)[1];
+                    }
+                    if (line.Contains("CircleSize:"))
+                    {
+                        circleSize = Convert.ToDouble(line.Split(':')[1]);
+                    }
+                    if (line.Contains("OverallDifficulty:"))
+                    {
+                        overallDifficulty = Convert.ToDouble(line.Split(':')[1]);
+                        approachRate = overallDifficulty;
+                    }
+                    if (line.Contains("ApproachRate:"))
+                    {
+                        approachRate = Convert.ToDouble(line.Split(':')[1]);
+                    }
+                    if (line.Contains("[HitObjects]"))
+                    {
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            int xCoordinate = Convert.ToInt32(line.Split(',')[0]);
+                            int yCoordinate = Convert.ToInt32(line.Split(',')[1]);
+                            int time = Convert.ToInt32(line.Split(',')[2]);
+                            int objectType = Convert.ToInt32(line.Split(',')[3]);
+                            Note note = new Note(xCoordinate, yCoordinate, time, objectType);
+                            if (note.objectType == 1 || note.objectType == 5)
+                            {
+                                circleCount++;
+                            }
+                            if (note.objectType != 12)
+                            {
+                                objectCount++;
+                                osuNotes.Add(note);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
