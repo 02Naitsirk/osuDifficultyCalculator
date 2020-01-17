c82c4c262abe5c736cc4f71bbb9fdc779a760d48
02Naitsirk <57512128+02Naitsirk@users.noreply.github.com>
2020-01-17 10:34:37 -0500

    implement sliders and use File class instead of StreamReader class

--- osuDifficultyCalculator/Beatmap.cs
+++ osuDifficultyCalculator/Beatmap.cs
@@ -6,7 +6,7 @@ namespace osuDifficultyCalculator
 {
     public class Beatmap
     {
-        public double circleSize, overallDifficulty, approachRate,
+        public double circleSize, overallDifficulty, approachRate, sliderTickRate,
             ezhtStarRating, nmhtStarRating, hrhtStarRating,
             ezStarRating, nmStarRating, hrStarRating,
             ezdtStarRating, nmdtStarRating, hrdtStarRating;
@@ -32,69 +32,96 @@ namespace osuDifficultyCalculator
         public struct Note
         {
             public int xCoordinate, yCoordinate, time, objectType;
-            public Note(int x, int y, int t, int o)
+            public double sliderDifficulty;
+            public Note(int x, int y, int t, int o, double sD)
             {
                 xCoordinate = x;
                 yCoordinate = y;
                 time = t;
                 objectType = o;
+                sliderDifficulty = sD;
             }
         }
 
         public void GetBeatmapData(string fileName)
         {
-            string line;
-            using StreamReader file = new StreamReader(fileName);
-            while ((line = file.ReadLine()) != null)
+            string[] allLines = File.ReadAllLines(fileName);
+            bool reachedHitObjectsLine = false;
+            for (int i = 0; i < allLines.Length; ++i)
             {
+                string line = allLines[i];
+                if (reachedHitObjectsLine)
+                {
+                    int xCoordinate = Convert.ToInt32(line.Split(',')[0]);
+                    int yCoordinate = Convert.ToInt32(line.Split(',')[1]);
+                    int time = Convert.ToInt32(line.Split(',')[2]);
+                    int nextTime = (i != allLines.Length - 1 ? Convert.ToInt32(allLines[i + 1].Split(',')[2]) : int.MaxValue);
+                    int objectType = Convert.ToInt32(line.Split(',')[3]);
+                    double sliderDifficulty = 0;
+                    if (objectType != 12)
+                    {
+                        objectCount++;
+                        if (!line.Contains("|"))
+                        {
+                            circleCount++;
+                        }
+                        else
+                        {
+                            int slideCount = Convert.ToInt32(line.Split(',')[6]);
+                            double travelDistance = Math.Max(0, Convert.ToDouble(line.Split(',')[7]) - 2 * Calculate.Diameter(circleSize));
+                            sliderDifficulty = sliderTickRate * slideCount * travelDistance / Math.Max(37.5, nextTime - time);
+                        }
+                        Note note = new Note(xCoordinate, yCoordinate, time, objectType, sliderDifficulty);
+                        osuNotes.Add(note);
+                    }
+                    continue;
+                }
                 if (line.Contains("Title:"))
                 {
                     title = line.Split(new[] { ':' }, 2)[1];
+                    continue;
                 }
                 if (line.Contains("Artist:"))
                 {
+                    Console.WriteLine(line);
                     artist = line.Split(new[] { ':' }, 2)[1];
+                    continue;
                 }
                 if (line.Contains("Creator:"))
                 {
                     creator = line.Split(new[] { ':' }, 2)[1];
+                    continue;
                 }
                 if (line.Contains("Version:"))
                 {
                     version = line.Split(new[] { ':' }, 2)[1];
+                    continue;
                 }
                 if (line.Contains("CircleSize:"))
                 {
                     circleSize = Convert.ToDouble(line.Split(':')[1]);
+                    continue;
                 }
                 if (line.Contains("OverallDifficulty:"))
                 {
                     overallDifficulty = Convert.ToDouble(line.Split(':')[1]);
                     approachRate = overallDifficulty;
+                    continue;
                 }
                 if (line.Contains("ApproachRate:"))
                 {
                     approachRate = Convert.ToDouble(line.Split(':')[1]);
+                    continue;
+                }
+                if (line.Contains("SliderTickRate:"))
+                {
+                    sliderTickRate = Convert.ToDouble(line.Split(':')[1]);
+                    continue;
                 }
                 if (line.Contains("[HitObjects]"))
                 {
-                    while ((line = file.ReadLine()) != null)
-                    {
-                        int xCoordinate = Convert.ToInt32(line.Split(',')[0]);
-                        int yCoordinate = Convert.ToInt32(line.Split(',')[1]);
-                        int time = Convert.ToInt32(line.Split(',')[2]);
-                        int objectType = Convert.ToInt32(line.Split(',')[3]);
-                        Note note = new Note(xCoordinate, yCoordinate, time, objectType);
-                        if (note.objectType != 12)
-                        {
-                            objectCount++;
-                            osuNotes.Add(note);
-                            if (!line.Contains('|'))
-                            {
-                                circleCount++;
-                            }
-                        }
-                    }
+                    reachedHitObjectsLine = true;
+                    continue;
                 }
             }
         }
