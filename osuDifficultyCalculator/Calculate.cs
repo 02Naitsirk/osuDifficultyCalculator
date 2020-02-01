e93e54e09e23f8e5a97703d97b74bb4abbd5d81d
02Naitsirk <57512128+02Naitsirk@users.noreply.github.com>
2020-02-01 10:34:50 -0500

    nerf lengthbonus, increase flexibility, remove static

--- osuDifficultyCalculator/Calculate.cs
+++ osuDifficultyCalculator/Calculate.cs
@@ -1,18 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using static osuDifficultyCalculator.Beatmap;
 
 namespace osuDifficultyCalculator
 {
-    public static class Calculate
+    public class Calculate
     {
         private const double consistencyMultiplier = 0.98;
         private const double starRatingMultiplier = 53.0;
         private const double starRatingExponent = 0.34;
         private const double ppMultiplier = 1.5;
         private const double ppExponent = 2.43;
-        public const double minimumTime = 37.5;
+        private const double minimumTime = 37.5;
 
         public enum Mods
         {
@@ -25,14 +24,14 @@ namespace osuDifficultyCalculator
         }
 
         /// Converts the circle size to its corresponding diameter.
-        public static double Diameter(double circleSize)
+        public double Diameter(double circleSize)
         {
             return 2 * (54.41 - 4.48 * circleSize);
         }
 
         /// A multiplicative pp bonus when the FL mod is enabled. The bonus increases as the object count increases.
         /// Minimum bonus is 1.0 at AR 10 or less. Maximum bonus is unbounded.
-        private static double FlashlightBonus(List<Note> beatmapObjects, int speedUpMod)
+        private double FlashlightBonus(List<Beatmap.Note> beatmapObjects, int speedUpMod)
         {
             double flBonus = 0;
             double timeDistanceRatio;
@@ -65,7 +64,7 @@ namespace osuDifficultyCalculator
 
         /// A multiplicative pp antibonus for hidden when the DT mod is enabled. The bonus decreases as the approach rate increases.
         /// Minimum bonus is 1.0 at AR 11; maximum bonus is 1.1 at AR 10.33 or less.
-        private static double HiddenBonus(double approachRate, int speedUpMod)
+        private double HiddenBonus(double approachRate, int speedUpMod)
         {
             double approachTime;
             switch (speedUpMod)
@@ -88,7 +87,7 @@ namespace osuDifficultyCalculator
 
         /// A multiplicative pp bonus for high AR when the DT mod is enabled. The bonus increases as the approach rate increases.
         /// Minimum bonus is 1.0 at AR 10 or less; maximum bonus is 1.25 at AR 11.
-        private static double HighApproachRateBonus(double approachRate, int speedUpMod)
+        private double HighApproachRateBonus(double approachRate, int speedUpMod)
         {
             double approachTime;
             switch (speedUpMod)
@@ -111,7 +110,7 @@ namespace osuDifficultyCalculator
 
         /// A multiplicative pp bonus for low AR. The bonus increases as the approach rate decreases.
         /// Minimum bonus is 1.0 at AR 11; maximum bonus is 2.0 at AR 0.
-        private static double LowApproachRateBonus(double approachRate, int speedUpMod)
+        private double LowApproachRateBonus(double approachRate, int speedUpMod)
         {
             double approachTime;
             switch (speedUpMod)
@@ -134,7 +133,7 @@ namespace osuDifficultyCalculator
 
         /// A multiplicative pp bonus for overall difficulty. The bonus increases as the overall difficulty increases.
         /// Minimum bonus is 1.0 at OD 0; maximum bonus is >2.0 at OD 11. 
-        private static double OverallDifficultyBonus(double overallDifficulty, int speedUpMod)
+        private double OverallDifficultyBonus(double overallDifficulty, int speedUpMod)
         {
             double hitWindow;
             switch (speedUpMod)
@@ -157,19 +156,19 @@ namespace osuDifficultyCalculator
 
         /// A multiplicative pp bonus for circle count. The bonus increases as the total amount of circles increases.
         /// Minimum bonus is 1.0 at 0 circles; maximum bonus is 3.0 at ∞ circles.
-        private static double LengthBonus(int circleCount)
+        private double LengthBonus(int circleCount)
         {
-            return 1 + 2.0 * circleCount / (circleCount + 2000);
+            return 1 + 1.8 * circleCount / (circleCount + 2000);
         }
 
         /// Calculates the distance between two notes.
-        public static double Distance(Note current, Note previous)
+        public double Distance(Beatmap.Note current, Beatmap.Note previous)
         {
             return Math.Sqrt(Math.Pow(current.xCoordinate - previous.xCoordinate, 2) + Math.Pow(current.yCoordinate - previous.yCoordinate, 2));
         }
 
         /// Calculates the angle between 3 notes. Return NaN if the angle does not exist.
-        public static double Angle(Note previousPrevious, Note previous, Note current)
+        public double Angle(Beatmap.Note previousPrevious, Beatmap.Note previous, Beatmap.Note current)
         {
             double previousToPreviousPrevious = Distance(previous, previousPrevious);
             double currentToPreviousPrevious = Distance(current, previousPrevious);
@@ -180,7 +179,7 @@ namespace osuDifficultyCalculator
         }
 
         /// Buff angle changes.
-        public static double AngleComplexityBuff(double angle, double previousAngle, double timeDifference, double previousTimeDifference)
+        public double AngleComplexityBuff(double angle, double previousAngle, double timeDifference, double previousTimeDifference)
         {
             timeDifference = Math.Max(minimumTime, timeDifference);
             previousTimeDifference = Math.Max(minimumTime, previousTimeDifference);
@@ -192,7 +191,7 @@ namespace osuDifficultyCalculator
         }
 
         /// Calculates the difficulty of a particular note.
-        public static double Difficulty(Note previousPreviousPreviousNote, Note previousPreviousNote, Note previousNote, Note currentNote, Note nextNote, int speedUpMod, double circleSize, double tickRate)
+        public double Difficulty(Beatmap.Note previousPreviousPreviousNote, Beatmap.Note previousPreviousNote, Beatmap.Note previousNote, Beatmap.Note currentNote, Beatmap.Note nextNote, int speedUpMod, double circleSize, double tickRate, bool doPrint = false)
         {
             double currentDistance = Distance(currentNote, previousNote);
             double overlap = Distance(currentNote, previousNote) / Diameter(circleSize);
@@ -207,39 +206,59 @@ namespace osuDifficultyCalculator
                     double dtSpeedBuff = Math.Max(1, 62.5 / dtTimeDifference);
                     double dtSliderDifficulty = Math.Max(0, tickRate * currentNote.slideCount * (currentNote.travelDistance - 2 * Diameter(circleSize))) / Math.Max(minimumTime, (nextNote.time - currentNote.time) / 1.5);
                     double baseDtDifficulty = (100 * currentDistance * (1 + dtSliderDifficulty) + currentDistance * (1 + dtSliderDifficulty) * dtTimeDifference) / Math.Pow(dtTimeDifference, 3);
-                    return baseDtDifficulty * dtSpeedBuff * (1 + overlapPunishment * AngleComplexityBuff(angle, previousAngle, dtTimeDifference, previousDtTimeDifference));
+                    double dtDifficulty = baseDtDifficulty * dtSpeedBuff * (1 + overlapPunishment * AngleComplexityBuff(angle, previousAngle, dtTimeDifference, previousDtTimeDifference));
+                    return dtDifficulty;
                 case 0:  /// NM
                     double previousNmTimeDifference = Math.Max(minimumTime, previousNote.time - previousPreviousNote.time);
                     double nmTimeDifference = Math.Max(minimumTime, currentNote.time - previousNote.time);
                     double nmSpeedBuff = Math.Max(1, 62.5 / nmTimeDifference);
                     double nmSliderDifficulty = Math.Max(0, tickRate * currentNote.slideCount * (currentNote.travelDistance - 2 * Diameter(circleSize))) / Math.Max(minimumTime, nextNote.time - currentNote.time);
                     double baseNmDifficulty = (100 * currentDistance * (1 + nmSliderDifficulty) + currentDistance * (1 + nmSliderDifficulty) * nmTimeDifference) / Math.Pow(nmTimeDifference, 3);
-                    return baseNmDifficulty * nmSpeedBuff * (1 + overlapPunishment * AngleComplexityBuff(angle, previousAngle, nmTimeDifference, previousNmTimeDifference));
+                    double nmDifficulty = baseNmDifficulty * nmSpeedBuff * (1 + overlapPunishment * AngleComplexityBuff(angle, previousAngle, nmTimeDifference, previousNmTimeDifference));
+                    return nmDifficulty;
                 case -1: /// HT
                     double previousHtTimeDifference = Math.Max(minimumTime, (previousNote.time - previousPreviousNote.time) / 0.75);
                     double htTimeDifference = Math.Max(minimumTime, (currentNote.time - previousNote.time) / 0.75);
                     double htSpeedBuff = Math.Max(1, 62.5 / htTimeDifference);
                     double htSliderDifficulty = Math.Max(0, tickRate * currentNote.slideCount * (currentNote.travelDistance - 2 * Diameter(circleSize))) / Math.Max(minimumTime, (nextNote.time - currentNote.time) / 0.75);
                     double baseHtDifficulty = (100 * currentDistance * (1 + htSliderDifficulty) + currentDistance * (1 + htSliderDifficulty) * htTimeDifference) / Math.Pow(htTimeDifference, 3);
-                    return baseHtDifficulty * htSpeedBuff * (1 + overlapPunishment * AngleComplexityBuff(angle, previousAngle, htTimeDifference, previousHtTimeDifference));
+                    double htDifficulty = baseHtDifficulty * htSpeedBuff * (1 + overlapPunishment * AngleComplexityBuff(angle, previousAngle, htTimeDifference, previousHtTimeDifference));
+                    return htDifficulty;
                 default:
                     return 0;
             }
         }
 
         /// Calculates the star rating of a beatmap.
-        public static double StarRating(List<double> difficulties, double circleSize)
+        public double StarRating(List<double> difficulties, double circleSize)
         {
-            difficulties = difficulties.OrderByDescending(x => x).ToList();
-            for (int i = 0; i < difficulties.Count; i++)
+            int sectionLength = 1;
+            int maximumMultiple = sectionLength * (difficulties.Count / sectionLength);
+            double currentSectionDifficulty = 0;
+            List<double> sectionDifficulties = new List<double>();
+            for (int i = 0; i < difficulties.Count; ++i)
             {
-                difficulties[i] *= Math.Pow(consistencyMultiplier, i);
+                currentSectionDifficulty += difficulties[i];
+                if (i != 0 && i % sectionLength == 0 && i < maximumMultiple)
+                {
+                    sectionDifficulties.Add(currentSectionDifficulty);
+                    currentSectionDifficulty = 0;
+                }
+                if (i >= maximumMultiple && i == difficulties.Count - 1)
+                {
+                    sectionDifficulties.Add(currentSectionDifficulty);
+                }
             }
-            return Math.Pow(difficulties.Sum(), starRatingExponent) * starRatingMultiplier / Math.Sqrt(Diameter(circleSize));
+            sectionDifficulties = sectionDifficulties.OrderByDescending(x => x).ToList();
+            for (int i = 0; i < sectionDifficulties.Count; i++)
+            {
+                sectionDifficulties[i] *= Math.Pow(consistencyMultiplier, i);
+            }
+            return Math.Pow(sectionDifficulties.Sum(), starRatingExponent) * starRatingMultiplier / Math.Sqrt(Diameter(circleSize));
         }
 
         /// Calculates the pp of a beatmap.
-        public static double PP(double starRating, double overallDifficulty, double approachRate, int circleCount, List<Note> osuNotes = null, int modEnum = 17)
+        public double PP(double starRating, double overallDifficulty, double approachRate, int circleCount, List<Beatmap.Note> osuNotes = null, int modEnum = 17)
         {
             double basePP = ppMultiplier * Math.Pow(starRating, ppExponent) * LengthBonus(circleCount);
             if (modEnum % (int)Mods.EZ == 0)
