9b6fe2ebfbb26b47b98ad2b42d63e56a93e9002c
02Naitsirk <57512128+02Naitsirk@users.noreply.github.com>
2020-03-18 12:29:19 -0400

    nerf extremely high strains, re-add angle buff, nerf short maps
    a higher value to sectionLength in StarRating() helps nerf maps like a hope in hell, lechenaultia, and other maps that have spaced bursts, but the consistencyMultiplier had to be lowered. the lower consistencyMultiplier made me readd the angle buff, add a short map nerf and buff anglechangebonus in order to balance the system.

--- osuDifficultyCalculator/Calculate.cs
+++ osuDifficultyCalculator/Calculate.cs
@@ -1,15 +1,15 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
+using static System.Math;
 
 namespace osuDifficultyCalculator
 {
     public class Calculate
     {
-        private const double consistencyMultiplier = 0.98;
-        private const double starRatingMultiplier = 53;
+        private const double consistencyMultiplier = 0.9;
+        private const double starRatingMultiplier = 56.25;
         private const double starRatingExponent = 0.34;
-        private const double ppMultiplier = 1.48;
+        private const double ppMultiplier = 1.5;
         private const double ppExponent = 2.43;
         private const double minimumTime = 37.5;
 
@@ -24,32 +24,9 @@ namespace osuDifficultyCalculator
         }
 
         /// Converts the circle size to its corresponding diameter.
-        private double Diameter(double circleSize)
+        private double ConvertCircleSize(double circleSize)
         {
             return 2 * (54.41 - 4.48 * circleSize);
-        }
-
-        /// Calculates the distance between two notes.
-        private double Distance(Beatmap.Note currentNote, Beatmap.Note lastNote)
-        {
-            return Math.Sqrt(Math.Pow(currentNote.xCoordinate - lastNote.xCoordinate, 2) + Math.Pow(currentNote.yCoordinate - lastNote.yCoordinate, 2));
-        }
-
-        ///  Calculate the note density at a note.
-        private int NoteDensity(List<Beatmap.Note> osuNotes, int notePosition, double approachRate, int speedUpMod)
-        {
-            int noteDensity = 1;
-            double speedUpFactor = speedUpMod == 0 ? 1.0 : speedUpMod == 1 ? 1.5 : 0.75;
-            double approachTime = ConvertApproachRate(approachRate, speedUpMod);
-            for (int i = notePosition; i + 1 < osuNotes.Count; ++i)
-            {
-                if ((osuNotes[i + 1].time - osuNotes[notePosition].time) / speedUpFactor >= approachTime)
-                {
-                    break;
-                }
-                noteDensity++;
-            }
-            return noteDensity;
         }
 
         /// Convert AR to milliseconds.
@@ -64,49 +41,81 @@ namespace osuDifficultyCalculator
             return approachTime;
         }
 
+        /// Calculates the distance between two notes.
+        private double Distance(Beatmap.Note currentNote, Beatmap.Note lastNote)
+        {
+            return Sqrt(Pow(currentNote.xCoordinate - lastNote.xCoordinate, 2) + Pow(currentNote.yCoordinate - lastNote.yCoordinate, 2));
+        }
+
+        /// Calculates the angle between 3 notes. Return NaN if the angle does not exist.
+        private double Angle(Beatmap.Note secondLastNote, Beatmap.Note lastNote, Beatmap.Note currentNote)
+        {
+            double lastToSecondLast = Distance(lastNote, secondLastNote);
+            double currentToSecondLast = Distance(currentNote, secondLastNote);
+            double currentToLast = Distance(currentNote, lastNote);
+            return currentToLast * lastToSecondLast > 0 ? Acos((Pow(lastToSecondLast, 2) + Pow(currentToLast, 2) - Pow(currentToSecondLast, 2)) / (2 * currentToLast * lastToSecondLast)) : double.NaN;
+        }
+
+        ///  Calculate the note density at a time.
+        private int NoteDensity(List<Beatmap.Note> osuNotes, int noteIndex, double approachRate, int speedUpMod)
+        {
+            int noteDensity = 1;
+            double speedUpFactor = speedUpMod == 0 ? 1.0 : speedUpMod == 1 ? 1.5 : 0.75;
+            double approachTime = ConvertApproachRate(approachRate, speedUpMod);
+            for (int i = noteIndex; i + 1 < osuNotes.Count; ++i)
+            {
+                if ((osuNotes[i + 1].time - osuNotes[noteIndex].time) / speedUpFactor >= approachTime)
+                {
+                    break;
+                }
+                noteDensity++;
+            }
+            return noteDensity;
+        }
+
         /// A multiplicative pp bonus when the FL mod is enabled. The bonus increases as the object count increases.
         /// Minimum bonus is 1.0 at AR 10 or less. Maximum bonus is unbounded.
         private double FlashlightBonus(List<Beatmap.Note> osuNotes, int speedUpMod)
         {
-            double flBonus = 0;
+            double flashlightBonus = 0;
             double timeDistanceRatio;
             double speedUpFactor = speedUpMod == 0 ? 1.0 : speedUpMod == 1 ? 1.5 : 0.75;
             for (int i = 1; i < osuNotes.Count; ++i)
             {
-                timeDistanceRatio = speedUpFactor * (Distance(osuNotes[i - 1], osuNotes[i]) + osuNotes[i].realTravelDistance) / Math.Max(minimumTime, osuNotes[i].time - osuNotes[i - 1].time);
-                flBonus += timeDistanceRatio;
+                timeDistanceRatio = speedUpFactor * (Distance(osuNotes[i - 1], osuNotes[i]) + osuNotes[i].realTravelDistance) / Max(minimumTime, osuNotes[i].time - osuNotes[i - 1].time);
+                flashlightBonus += timeDistanceRatio;
             }
-            return Math.Pow(1 + Math.Sqrt(flBonus) / 125, 2);
+            return Pow(1 + Sqrt(flashlightBonus) / 125, 2);
         }
 
-        /// A multiplicative pp antibonus for hidden when the DT mod is enabled. The bonus increases as the note density increases.
+        /// A multiplicative pp nerf for hidden when the DT mod is enabled. The bonus increases as the note density increases.
         private double HiddenBonus(List<Beatmap.Note> osuNotes, double approachRate, double circleSize, int speedUpMod)
         {
-            double hiddenBonusSum = 0;
+            double hiddenBonus = 0;
             for (int i = 0; i < osuNotes.Count; ++i)
             {
                 double stackNerf = 1;
-                int currentNoteDensity = Math.Min(NoteDensity(osuNotes, i, approachRate, speedUpMod), 26);
+                int currentNoteDensity = Min(NoteDensity(osuNotes, i, approachRate, speedUpMod), 26);
 
-                /// Nerf stacks and low-spaced streams.
-                if (i > 0 && Distance(osuNotes[i], osuNotes[i - 1]) / Diameter(circleSize) < 0.5)
+                /// Nerf stacks and low-spaced streams. Prevents maps like Ascension to Heaven from becoming overbuffed.
+                if (i > 0 && Distance(osuNotes[i], osuNotes[i - 1]) / ConvertCircleSize(circleSize) < 0.5)
                 {
-                    stackNerf = Math.Pow(2 * Distance(osuNotes[i], osuNotes[i - 1]) / Diameter(circleSize), 4);
+                    stackNerf = Pow(2 * Distance(osuNotes[i], osuNotes[i - 1]) / ConvertCircleSize(circleSize), 4);
                 }
-                hiddenBonusSum += 1 + Math.Pow(Math.Sin(Math.PI * currentNoteDensity / 52), 2) * stackNerf;
+                hiddenBonus += 1 + Pow(Sin(PI * currentNoteDensity / 52), 2) * stackNerf;
             }
-            return hiddenBonusSum / osuNotes.Count;
+            return hiddenBonus / osuNotes.Count;
         }
 
         /// A multiplicative pp bonus for high AR when the DT mod is enabled. The bonus increases as the approach rate increases.
-        /// Minimum bonus is 1.0 at AR 10 or less; maximum bonus is 1.25 at AR 11.
+        /// Minimum bonus is 1.0 at AR 10 or less; maximum bonus is 1.2 at AR 11.
         private double HighApproachRateBonus(double approachRate, int speedUpMod)
         {
             double approachTime = ConvertApproachRate(approachRate, speedUpMod);
-            return 1 + 0.8 * Math.Pow((Math.Min(approachTime, 450) - 450) / 300, 2);
+            return 1 + 0.8 * Pow((Min(approachTime, 450) - 450) / 300, 2);
         }
 
-        /// Buff high density (low AR); doesn't buff high AR stream maps.
+        /// Buff high density (low AR); minimum buff to high AR stream maps.
         private double HighDensityBonus(List<Beatmap.Note> osuNotes, double approachRate, int speedUpMod)
         {
             double highDensityBonus = 0;
@@ -125,7 +134,7 @@ namespace osuDifficultyCalculator
                     }
                 }
             }
-            return 1 + 0.5 * Math.Pow(highDensityBonus / (osuNotes.Count * 1200), 2);
+            return 1 + 0.5 * Pow(highDensityBonus / (osuNotes.Count * 1200), 2);
         }
 
         /// A multiplicative pp bonus for overall difficulty. The bonus increases as the overall difficulty increases.
@@ -134,7 +143,7 @@ namespace osuDifficultyCalculator
         {
             double speedUpFactor = speedUpMod == 0 ? 1.0 : speedUpMod == 1 ? 1.5 : 0.75;
             double hitWindow = (79.5 - 6 * overallDifficulty) / speedUpFactor;
-            return 2 * Math.Pow(2, (19.5 - hitWindow) / 60);
+            return 2 * Pow(2, (19.5 - hitWindow) / 60);
         }
 
         /// A multiplicative pp bonus for circle count. The bonus increases as the total amount of circles increases.
@@ -143,53 +152,57 @@ namespace osuDifficultyCalculator
         {
             return 1 + 1.8 * circleCount / (circleCount + 2000);
         }
-
-        /// Calculates the angle between 3 notes. Return NaN if the angle does not exist.
-        private double Angle(Beatmap.Note secondLastNote, Beatmap.Note lastNote, Beatmap.Note currentNote)
+        
+        /// Nerf short maps with object count less than 500 objects.
+        private double ShortMapNerf(int objectCount)
         {
-            double lastToSecondLast = Distance(lastNote, secondLastNote);
-            double currentToSecondLast = Distance(currentNote, secondLastNote);
-            double currentToLast = Distance(currentNote, lastNote);
-            return currentToLast * lastToSecondLast > 0
-                ? Math.Acos((Math.Pow(lastToSecondLast, 2) + Math.Pow(currentToLast, 2) - Math.Pow(currentToSecondLast, 2)) / (2 * currentToLast * lastToSecondLast))
-                : double.NaN;
+            return 1 - Pow((double) Min(objectCount, 500) / 500 - 1, 2) / 10;
         }
 
         /// Buff angle changes.
-        private double AngleChangeBuff(double angle, double lastAngle, double timeDifference, double lastTimeDifference)
+        private double AngleChangeBonus(double angle, double lastAngle, double timeDifference, double lastTimeDifference)
         {
-            lastTimeDifference = Math.Max(minimumTime, lastTimeDifference);
-            timeDifference = Math.Max(minimumTime, timeDifference);
-            double timeDifferenceNerf = Math.Pow(10, -Math.Pow(timeDifference - lastTimeDifference, 2) / 1000);
-            double scaledAngle = Math.Pow(Math.Cos(angle / 2), 2);
-            double scaledLastAngle = Math.Pow(Math.Cos(lastAngle / 2), 2);
-            double angleDifference = Math.Abs(scaledAngle - scaledLastAngle);
-            return double.IsNaN(angleDifference) ? 0 : timeDifferenceNerf * angleDifference / 2;
+            double timeDifferenceNerf = Pow(10, -Pow(timeDifference - lastTimeDifference, 2) / 1000);
+            double scaledAngle = Pow(Cos(angle / 2), 2);
+            double scaledLastAngle = Pow(Cos(lastAngle / 2), 2);
+            double angleDifference = Abs(scaledAngle - scaledLastAngle);
+            return double.IsNaN(angleDifference) ? 0 : timeDifferenceNerf * angleDifference;
+        }
+
+        ///  Buff wide angles.
+        private double AngleBonus(double angle, double currentDistance, double timeDifference, double lastTimeDifference)
+        {
+            double timeDifferenceNerf = Pow(10, -Pow(timeDifference - lastTimeDifference, 2) / 1000);
+            /// Instead of reducing the wide angle effect on high BPMs, the distance between notes must be large enough for the angle buff to be significant. 
+            /// This is to prevent spaced streams from being buffed or nerfed too much, whilst also buffing fast, spaced, wide-angle patterns (Disconnected Hardkore DT).
+            double lowDistanceNerf = Min(1, Pow(currentDistance / (640 * 6.4 * Pow(2, -0.05 * timeDifference)), 4));
+            double wideAngleBuff = Pow(Sin(angle / 2), 2);
+            return double.IsNaN(wideAngleBuff) ? 0 : wideAngleBuff * lowDistanceNerf * timeDifferenceNerf / 3;
         }
 
         /// Calculates the difficulty of a particular note.
         public double Difficulty(Beatmap.Note thirdLastNote, Beatmap.Note secondLastNote, Beatmap.Note lastNote, Beatmap.Note currentNote, Beatmap.Note nextNote, int speedUpMod, double circleSize, double tickRate, bool doPrint = false)
         {
-            double currentDistance = Distance(currentNote, lastNote) + 10 * Math.Cos(Math.PI * Math.Min(Distance(currentNote, lastNote), 25) / 50);
-            double overlap = currentDistance / Diameter(circleSize);
-            double overlapNerf = Math.Min(1, Math.Pow(overlap, 2));
+            double currentDistance = Distance(currentNote, lastNote) + 10 * Cos(PI * Min(Distance(currentNote, lastNote), 25) / 50);
+            double overlap = currentDistance / ConvertCircleSize(circleSize);
+            double overlapNerf = Min(1, Pow(overlap, 2));
             double angle = Angle(secondLastNote, lastNote, currentNote);
             double lastAngle = Angle(thirdLastNote, secondLastNote, lastNote);
             double speedUpFactor = speedUpMod == 0 ? 1.0 : speedUpMod == 1 ? 1.5 : 0.75;
 
-            double lastTimeDifference = Math.Max(minimumTime, (lastNote.time - secondLastNote.time) / speedUpFactor);
-            double timeDifference = Math.Max(minimumTime, (currentNote.time - lastNote.time) / speedUpFactor);
-            double speedBuff = Math.Max(1, 2 - 3 * timeDifference / 200);
-            double sliderDifficulty = 1 + Math.Max(0, tickRate * currentNote.realTravelDistance) / Math.Max(minimumTime, (nextNote.time - currentNote.time) / speedUpFactor);
-            double baseDifficulty = (100 * currentDistance * sliderDifficulty + currentDistance * sliderDifficulty * timeDifference) / Math.Pow(timeDifference, 3);
-            double difficulty = baseDifficulty * speedBuff * (1 + overlapNerf * AngleChangeBuff(angle, lastAngle, timeDifference, lastTimeDifference));
+            double timeDifference = Max(minimumTime, (currentNote.time - lastNote.time) / speedUpFactor);
+            double lastTimeDifference = Max(minimumTime, (lastNote.time - secondLastNote.time) / speedUpFactor);
+            double speedBuff = Max(1, 2 - 3 * timeDifference / 200);
+            double sliderDifficulty = 1 + Max(0, tickRate * currentNote.realTravelDistance) / Max(minimumTime, (nextNote.time - currentNote.time) / speedUpFactor);
+            double baseDifficulty = (100 * currentDistance * sliderDifficulty + currentDistance * sliderDifficulty * timeDifference) / Pow(timeDifference, 3);
+            double difficulty = baseDifficulty * speedBuff * (1 + overlapNerf * AngleChangeBonus(angle, lastAngle, timeDifference, lastTimeDifference)) * (1 + overlapNerf * AngleBonus(angle, currentDistance, timeDifference, lastTimeDifference));
             return difficulty;
         }
 
         /// Calculates the star rating of a beatmap.
         public double StarRating(List<double> difficulties, double circleSize)
         {
-            int sectionLength = 1;
+            int sectionLength = 4;
             int maximumMultiple = sectionLength * (difficulties.Count / sectionLength);
             double currentSectionDifficulty = 0;
             List<double> sectionDifficulties = new List<double>();
@@ -209,15 +222,15 @@ namespace osuDifficultyCalculator
             sectionDifficulties = sectionDifficulties.OrderByDescending(x => x).ToList();
             for (int i = 0; i < sectionDifficulties.Count; ++i)
             {
-                sectionDifficulties[i] *= Math.Pow(consistencyMultiplier, i);
+                sectionDifficulties[i] *= Pow(consistencyMultiplier, i);
             }
-            return Math.Pow(sectionDifficulties.Sum(), starRatingExponent) * starRatingMultiplier / Math.Sqrt(Diameter(circleSize));
+            return Pow(sectionDifficulties.Sum(), starRatingExponent) * starRatingMultiplier / Sqrt(ConvertCircleSize(circleSize)) * ShortMapNerf(difficulties.Count);
         }
 
         /// Calculates the pp of a beatmap.
         public double PP(double starRating, double overallDifficulty, double approachRate, double circleSize, int circleCount, List<Beatmap.Note> osuNotes, int modEnum = 17)
         {
-            double basePP = ppMultiplier * Math.Pow(starRating, ppExponent) * LengthBonus(circleCount);
+            double basePP = ppMultiplier * Pow(starRating, ppExponent) * LengthBonus(circleCount);
             if (modEnum % (int)Mods.EZ == 0)
             {
                 overallDifficulty /= 2;
@@ -225,8 +238,8 @@ namespace osuDifficultyCalculator
             }
             if (modEnum % (int)Mods.HR == 0)
             {
-                overallDifficulty = Math.Min(10, overallDifficulty * 1.4);
-                approachRate = Math.Min(10, approachRate * 1.4);
+                overallDifficulty = Min(10, overallDifficulty * 1.4);
+                approachRate = Min(10, approachRate * 1.4);
             }
             if (modEnum % (int)Mods.DT == 0)
             {
@@ -264,7 +277,7 @@ namespace osuDifficultyCalculator
                 }
                 basePP *= OverallDifficultyBonus(overallDifficulty, 0) * HighApproachRateBonus(approachRate, 0) * HighDensityBonus(osuNotes, approachRate, 0);
             }
-            return Math.Round(basePP, 2);
+            return Round(basePP, 2);
         }
     }
 }
