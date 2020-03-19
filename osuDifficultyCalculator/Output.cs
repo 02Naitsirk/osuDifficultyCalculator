11865c13fb4eb154f2e40416285c3d171fd1e90d
02Naitsirk <57512128+02Naitsirk@users.noreply.github.com>
2020-03-18 23:08:52 -0400

    reduce high strain punishment

--- osuDifficultyCalculator/Output.cs
+++ osuDifficultyCalculator/Output.cs
@@ -1,4 +1,5 @@
 using System;
+using static System.Math;
 
 namespace osuDifficultyCalculator
 {
@@ -22,23 +23,23 @@ namespace osuDifficultyCalculator
                     /// Calculate difficulty for every note.
                     for (int i = 1; i < beatmap.osuNotes.Count; i++)
                     {
-                        Beatmap.Note thirdLastNote = i - 3 > -1 ? beatmap.osuNotes[i - 3] : new Beatmap.Note(beatmap.osuNotes[i].xCoordinate, beatmap.osuNotes[i].yCoordinate, double.NegativeInfinity, 0);
-                        Beatmap.Note secondLastNote = i - 2 > -1 ? beatmap.osuNotes[i - 2] : new Beatmap.Note(beatmap.osuNotes[i].xCoordinate, beatmap.osuNotes[i].yCoordinate, double.MinValue, 0);
-                        Beatmap.Note lastNote = i - 1 > -1 ? beatmap.osuNotes[i - 1] : new Beatmap.Note(beatmap.osuNotes[i].xCoordinate, beatmap.osuNotes[i].yCoordinate, int.MinValue, 0);
+                        Beatmap.Note thirdLastNote = beatmap.osuNotes[Max(0, i - 3)];
+                        Beatmap.Note secondLastNote = beatmap.osuNotes[Max(0, i - 2)];
+                        Beatmap.Note lastNote = beatmap.osuNotes[i - 1];
                         Beatmap.Note currentNote = beatmap.osuNotes[i];
-                        Beatmap.Note nextNote = i + 1 < beatmap.osuNotes.Count ? beatmap.osuNotes[i + 1] : new Beatmap.Note(beatmap.osuNotes[i].xCoordinate, beatmap.osuNotes[i].yCoordinate, double.PositiveInfinity, 0);
+                        Beatmap.Note nextNote = i + 1 < beatmap.osuNotes.Count ? beatmap.osuNotes[i + 1] : new Beatmap.Note(beatmap.osuNotes[i].xCoordinate, beatmap.osuNotes[i].yCoordinate, int.MaxValue, 0);
 
                         double ezhtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, -1, beatmap.circleSize / 2, beatmap.sliderTickRate);
                         double nmhtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, -1, beatmap.circleSize, beatmap.sliderTickRate);
-                        double hrhtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, -1, Math.Min(10, beatmap.circleSize * 1.3), beatmap.sliderTickRate);
+                        double hrhtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, -1, Min(10, beatmap.circleSize * 1.3), beatmap.sliderTickRate);
 
                         double ezDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 0, beatmap.circleSize / 2, beatmap.sliderTickRate);
                         double nmDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 0, beatmap.circleSize, beatmap.sliderTickRate);
-                        double hrDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 0, Math.Min(10, beatmap.circleSize * 1.3), beatmap.sliderTickRate);
+                        double hrDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 0, Min(10, beatmap.circleSize * 1.3), beatmap.sliderTickRate);
 
                         double ezdtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 1, beatmap.circleSize / 2, beatmap.sliderTickRate);
                         double nmdtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 1, beatmap.circleSize, beatmap.sliderTickRate);
-                        double hrdtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 1, Math.Min(10, beatmap.circleSize * 1.3), beatmap.sliderTickRate);
+                        double hrdtDifficulty = calculate.Difficulty(thirdLastNote, secondLastNote, lastNote, currentNote, nextNote, 1, Min(10, beatmap.circleSize * 1.3), beatmap.sliderTickRate);
 
                         beatmap.ezhtDifficulties.Add(ezhtDifficulty);
                         beatmap.nmhtDifficulties.Add(nmhtDifficulty);
@@ -68,41 +69,41 @@ namespace osuDifficultyCalculator
                         double nmdtProportion = beatmap.nmdtDifficulties[i] > 0 ? beatmap.nmdtDifficulties[i - 1] / beatmap.nmdtDifficulties[i] : double.PositiveInfinity;
                         double hrdtProportion = beatmap.hrdtDifficulties[i] > 0 ? beatmap.hrdtDifficulties[i - 1] / beatmap.hrdtDifficulties[i] : double.PositiveInfinity;
 
-                        const int h = 4;
+                        const int h = 3;
                         if (ezhtProportion < 0.5)
-                            beatmap.ezhtDifficulties[i] *= 1 - h * Math.Pow(ezhtProportion - 0.5, 2);
+                            beatmap.ezhtDifficulties[i] *= 1 - h * Pow(ezhtProportion - 0.5, 2);
                         if (nmhtProportion < 0.5)
-                            beatmap.nmhtDifficulties[i] *= 1 - h * Math.Pow(nmhtProportion - 0.5, 2);
+                            beatmap.nmhtDifficulties[i] *= 1 - h * Pow(nmhtProportion - 0.5, 2);
                         if (hrhtProportion < 0.5)
-                            beatmap.hrhtDifficulties[i] *= 1 - h * Math.Pow(hrhtProportion - 0.5, 2);
+                            beatmap.hrhtDifficulties[i] *= 1 - h * Pow(hrhtProportion - 0.5, 2);
 
                         if (ezProportion < 0.5)
-                            beatmap.ezDifficulties[i] *= 1 - h * Math.Pow(ezProportion - 0.5, 2);
+                            beatmap.ezDifficulties[i] *= 1 - h * Pow(ezProportion - 0.5, 2);
                         if (nmProportion < 0.5)
-                            beatmap.nmDifficulties[i] *= 1 - h * Math.Pow(nmProportion - 0.5, 2);
+                            beatmap.nmDifficulties[i] *= 1 - h * Pow(nmProportion - 0.5, 2);
                         if (hrProportion < 0.5)
-                            beatmap.hrDifficulties[i] *= 1 - h * Math.Pow(hrProportion - 0.5, 2);
+                            beatmap.hrDifficulties[i] *= 1 - h * Pow(hrProportion - 0.5, 2);
 
                         if (ezdtProportion < 0.5)
-                            beatmap.ezdtDifficulties[i] *= 1 - h * Math.Pow(ezdtProportion - 0.5, 2);
+                            beatmap.ezdtDifficulties[i] *= 1 - h * Pow(ezdtProportion - 0.5, 2);
                         if (nmdtProportion < 0.5)
-                            beatmap.nmdtDifficulties[i] *= 1 - h * Math.Pow(nmdtProportion - 0.5, 2);
+                            beatmap.nmdtDifficulties[i] *= 1 - h * Pow(nmdtProportion - 0.5, 2);
                         if (hrdtProportion < 0.5)
-                            beatmap.hrdtDifficulties[i] *= 1 - h * Math.Pow(hrdtProportion - 0.5, 2);
+                            beatmap.hrdtDifficulties[i] *= 1 - h * Pow(hrdtProportion - 0.5, 2);
                     }
 
                     /// Calculates star rating and pp.
                     beatmap.ezhtStarRating = calculate.StarRating(beatmap.ezhtDifficulties, beatmap.circleSize / 2);
                     beatmap.nmhtStarRating = calculate.StarRating(beatmap.nmhtDifficulties, beatmap.circleSize);
-                    beatmap.hrhtStarRating = calculate.StarRating(beatmap.hrhtDifficulties, Math.Min(10, beatmap.circleSize * 1.3));
+                    beatmap.hrhtStarRating = calculate.StarRating(beatmap.hrhtDifficulties, Min(10, beatmap.circleSize * 1.3));
 
                     beatmap.ezStarRating = calculate.StarRating(beatmap.ezDifficulties, beatmap.circleSize / 2);
                     beatmap.nmStarRating = calculate.StarRating(beatmap.nmDifficulties, beatmap.circleSize);
-                    beatmap.hrStarRating = calculate.StarRating(beatmap.hrDifficulties, Math.Min(10, beatmap.circleSize * 1.3));
+                    beatmap.hrStarRating = calculate.StarRating(beatmap.hrDifficulties, Min(10, beatmap.circleSize * 1.3));
 
                     beatmap.ezdtStarRating = calculate.StarRating(beatmap.ezdtDifficulties, beatmap.circleSize / 2);
                     beatmap.nmdtStarRating = calculate.StarRating(beatmap.nmdtDifficulties, beatmap.circleSize);
-                    beatmap.hrdtStarRating = calculate.StarRating(beatmap.hrdtDifficulties, Math.Min(10, beatmap.circleSize * 1.3));
+                    beatmap.hrdtStarRating = calculate.StarRating(beatmap.hrdtDifficulties, Min(10, beatmap.circleSize * 1.3));
 
                     double nm = calculate.PP(beatmap.nmStarRating, beatmap.overallDifficulty, beatmap.approachRate, beatmap.circleSize, beatmap.circleCount, beatmap.osuNotes);
 
@@ -150,17 +151,17 @@ namespace osuDifficultyCalculator
                     Console.WriteLine($"CS: {beatmap.circleSize}, OD: {beatmap.overallDifficulty}, AR: {beatmap.approachRate}\n");
                     Console.WriteLine($"{"Mods",-10} {"Stars",-10} {"NM pp",-10} {"HD pp",-10} {"FL pp",-10} {"FLHD pp",-10}\n");
 
-                    Console.WriteLine($"{"EZHT",-10} {Math.Round(beatmap.ezhtStarRating, 2),-10} {ezht,-10} {ezhdht,-10} {ezflht,-10} {ezflhdht,-10}");
-                    Console.WriteLine($"{"HT",-10} {Math.Round(beatmap.nmhtStarRating, 2),-10} {ht,-10} {hdht,-10} {flht,-10} {flhdht,-10}");
-                    Console.WriteLine($"{"HRHT",-10} {Math.Round(beatmap.hrhtStarRating, 2),-10} {hrht,-10} {hdhrht,-10} {flhrht,-10} {flhdhrht,-10}\n");
+                    Console.WriteLine($"{"EZHT",-10} {Round(beatmap.ezhtStarRating, 2),-10} {ezht,-10} {ezhdht,-10} {ezflht,-10} {ezflhdht,-10}");
+                    Console.WriteLine($"{"HT",-10} {Round(beatmap.nmhtStarRating, 2),-10} {ht,-10} {hdht,-10} {flht,-10} {flhdht,-10}");
+                    Console.WriteLine($"{"HRHT",-10} {Round(beatmap.hrhtStarRating, 2),-10} {hrht,-10} {hdhrht,-10} {flhrht,-10} {flhdhrht,-10}\n");
 
-                    Console.WriteLine($"{"EZ",-10} {Math.Round(beatmap.ezStarRating, 2),-10} {ez,-10} {ezhd,-10} {ezfl,-10} {ezflhd,-10}");
-                    Console.WriteLine($"{"NM",-10} {Math.Round(beatmap.nmStarRating, 2),-10} {nm,-10} {hd,-10} {fl,-10} {flhd,-10}");
-                    Console.WriteLine($"{"HR",-10} {Math.Round(beatmap.hrStarRating, 2),-10} {hr,-10} {hdhr,-10} {flhr,-10} {flhdhr,-10}\n");
+                    Console.WriteLine($"{"EZ",-10} {Round(beatmap.ezStarRating, 2),-10} {ez,-10} {ezhd,-10} {ezfl,-10} {ezflhd,-10}");
+                    Console.WriteLine($"{"NM",-10} {Round(beatmap.nmStarRating, 2),-10} {nm,-10} {hd,-10} {fl,-10} {flhd,-10}");
+                    Console.WriteLine($"{"HR",-10} {Round(beatmap.hrStarRating, 2),-10} {hr,-10} {hdhr,-10} {flhr,-10} {flhdhr,-10}\n");
 
-                    Console.WriteLine($"{"EZDT",-10} {Math.Round(beatmap.ezdtStarRating, 2),-10} {dtez,-10} {dtezhd,-10} {dtezfl,-10} {dtezflhd,-10}");
-                    Console.WriteLine($"{"DT",-10} {Math.Round(beatmap.nmdtStarRating, 2),-10} {dt,-10} {dthd,-10} {dtfl,-10} {dtflhd,-10}");
-                    Console.WriteLine($"{"HRDT",-10} {Math.Round(beatmap.hrdtStarRating, 2),-10} {dthr,-10} {dthdhr,-10} {dtflhr,-10} {dtflhdhr,-10}\n");
+                    Console.WriteLine($"{"EZDT",-10} {Round(beatmap.ezdtStarRating, 2),-10} {dtez,-10} {dtezhd,-10} {dtezfl,-10} {dtezflhd,-10}");
+                    Console.WriteLine($"{"DT",-10} {Round(beatmap.nmdtStarRating, 2),-10} {dt,-10} {dthd,-10} {dtfl,-10} {dtflhd,-10}");
+                    Console.WriteLine($"{"HRDT",-10} {Round(beatmap.hrdtStarRating, 2),-10} {dthr,-10} {dthdhr,-10} {dtflhr,-10} {dtflhdhr,-10}\n");
 
                     /// Reset objects in preparation for the next beatmap.
                     beatmap.osuNotes.Clear();
